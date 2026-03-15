a783199b07dd89162ab44550ca519e2c3220f003
MAhmad25 <userarenotproduct@gmail.com>
2026-03-15 23:36:49 +0500

    add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,25 @@ import AppRoute from "./Routes/AppRoute";
 import PostLoader from "./components/ui/PostLoader";
 import { useSelector } from "react-redux";
 import { StaggeredMenu } from "./components/index";
+import { ReactLenis, useLenis } from "lenis/react";
 const App = () => {
       const isLoading = useSelector((state) => state.loader.isLoading);
+      useLenis();
       const menuItems = [
             { label: "Home", ariaLabel: "Go to home page", link: "/" },
             { label: "Journals", ariaLabel: "Read the journals", link: "/journals" },
       ];
       return (
-            <Container>
-                  {isLoading && <PostLoader />}
-                  <div className="fixed sm:hidden w-screen pointer-events-none z-[9999]  h-screen overflow-hidden">
-                        <StaggeredMenu items={menuItems} menuButtonColor="#000000" openMenuButtonColor="#000000" />
-                  </div>
-                  <AppRoute />
-            </Container>
+            <>
+                  <ReactLenis root />
+                  <Container>
+                        {isLoading && <PostLoader />}
+                        <div className="fixed sm:hidden w-screen pointer-events-none z-[9999]  h-screen overflow-hidden">
+                              <StaggeredMenu items={menuItems} menuButtonColor="#000000" openMenuButtonColor="#000000" />
+                        </div>
+                        <AppRoute />
+                  </Container>
+            </>
       );
 };
 
